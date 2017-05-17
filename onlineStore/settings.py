7db1a9ca394761d0eba0a07187448612545e345a
popsonebz <popoolaebenezer@gmail.com>
2017-05-17 21:45:50 +0200

    changed database

--- onlineStore/settings.py
+++ onlineStore/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'onlineStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': '172.18.0.2',
+        'PORT': '',
     }
 }
 
