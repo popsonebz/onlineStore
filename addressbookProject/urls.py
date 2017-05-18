5c37aba8ed4a787855c13c9cb49cb9d4bdd56464
popsonebz <popoolaebenezer@gmail.com>
2017-05-19 00:43:41 +0200

    initial commit

--- addressbookProject/urls.py
+++ addressbookProject/urls.py
@@ -1,4 +1,4 @@
-"""onlineStore URL Configuration
+"""addressbookProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^mystore/', include('mystore.urls')),
     url(r'^addressbook/', include('AddressBookClient.urls')),
     url(r'^addressbook-admin/', include('AddressBookBackEnd.urls')),
     url(r'^admin/', admin.site.urls),
--- addressbookProject/wsgi.py
+++ addressbookProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for onlineStore project.
+WSGI config for addressbookProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onlineStore.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "addressbookProject.settings")
 
 application = get_wsgi_application()
